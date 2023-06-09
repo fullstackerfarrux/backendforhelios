10130bedec28109d64cf1c093524371706cfe82c
farrux <orziyevfarrux01@gmail.com>
2023-06-09 18:24:08 +0500

    test

--- Controller/users.controller.js
+++ Controller/users.controller.js
@@ -23,11 +23,15 @@ const postUser = async (req, res) => {
       "INSERT INTO allusers(user_id, tg_username, phone_number, users_location, user_language) values($1, $2, $3, $4, $5)",
       [user_id, username, phone_number, users_location, user_language]
     );
+
+    res.status(200).send({ msg: "created" });
   } else {
     let update = await client.query(
       "UPDATE allusers SET users_location = $1 where user_id = $2",
       [users_location, user_id]
     );
+
+    res.status(200).send({ msg: "updated" });
   }
 };
 
