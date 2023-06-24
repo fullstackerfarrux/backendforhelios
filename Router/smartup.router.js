7f50b3b41f5d1fa8387a659b8b724824ea4345a4
farrux <orziyevfarrux01@gmail.com>
2023-06-24 16:16:18 +0500

    test

--- Router/smartup.router.js
+++ Router/smartup.router.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import getOneByNumber from "../ControllerForSmartUp/getOneByNumber.controller.js";
-// import createUser from "../ControllerForSmartUp/createUser.controller.js";
 import createOrder from "../ControllerForSmartUp/createOrder.controller.js";
+import createUser from "../ControllerForSmartUp/createUSer.controller.js";
 
 const router = Router();
 
 router.post("/smartup/getByPhone", getOneByNumber);
-// router.post("/smartup/createUser", createUser);
+router.post("smartup/createUser", createUser);
 router.post("/smartup/createOrder", createOrder);
 
 export default router;
