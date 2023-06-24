8e79ff8ee7bb83f953c076efd33e06495ae4c6ee
farrux <orziyevfarrux01@gmail.com>
2023-06-24 16:19:51 +0500

    fixed createuser api

--- ControllerForSmartUp/createUSer.controller.js
+++ ControllerForSmartUp/createUSer.controller.js
@@ -10,72 +10,20 @@ const createUser = async (req, res) => {
     "Basic YWRtaW5Aa2FpemVuZ3JvdXA6Z3JlZW53aGl0ZQ=="
   );
 
-  //   var raw = `{
-  //     "legal_person": [{
-  //         "name":            "${first_name}",
-  //         "short_name":      "${first_name}",
-  //         "code":            "${phone_number}",
-  //         "email":           "${email}",
-  //         "gender":          "M",
-  //         "is_budgetarian":  "Y",
-  //         "state":           "A",
-  //         "main_phone":      "+998${phone_number}",
-  //         "tin":             "${phone_number}"
-  //     }]
-  // }`;
-
   var raw = `{
-   "legal_person": [
-    {
-      "name": "farrux7",
-      "code": "913152006",
-      "short_name": "farux",
-      "region_code": "Tashkent",
-      "is_budgetarian": "Y",
-      "tin": "965234875",
-      "state": "A",
-      "primary_person_code": "",
-      "parent_person_code": "",
-      "barcode": "48465198",
-      "vat_code": "566419465165",
-      "cea": "",
-      "main_phone": "+998913152006",
-      "email": "farrux@gmail.com",
-      "web": "",
-      "address": "Tashkent, Beshagach 1",
-      "address_guide": "",
-      "zip_code": "32105",
-      "latlng": "",
-      "is_client": "",
-      "is_supplier": "",
-      "groups": [
-        {
-          "group_code": "",
-          "type_code": ""
-        }
-      ],
-       "bank_accounts": [
- {
-          "bank_account_id": "",
-          "bank_account_code": "",
-          "bank_account_name": "",
-          "is_main": "",
-          "currency_code": "",
-          "state": "",
-          "note": "",
-          "mfo": ""
-        
- }
-],
- "rooms": [
-        {
-          "room_code": ""
-        }
-      ]
-    }
-  ]
-}
-}`;
+      "legal_person": [{
+          "name":            "${first_name}",
+          "short_name":      "${first_name}",
+          "code":            "${phone_number}",
+          "email":           "${email}",
+          "gender":          "M",
+          "is_budgetarian":  "Y",
+          "state":           "A",
+          "main_phone":      "+998${phone_number}",
+          "is_client":       "Y"
+      }]
+  }`;
+
   var requestOptions = {
     method: "POST",
     headers: myHeaders,
